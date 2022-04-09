81a89e33d53dbfdf51701f14c69aaa5958a982ef
Mouzey406 <muuo947@gmail.com>
2022-04-09 19:41:56 +0300

    Updated Script.js

--- script.js
+++ script.js
@@ -4,23 +4,26 @@
 const dataOpeners = document.querySelectorAll('[data-pop]');
 const dataClose = document.querySelectorAll('[data-closes]');
 const navPop = document.querySelectorAll('.nav-pop');
+const popWrapper = document.querySelector(".pop-wrapper");
 // const closeBtn = document.querySelector('.icon-close');
 
 dataOpeners.forEach((clk) => {
   clk.addEventListener('click', function (g) {
+    let activePops = document.querySelectorAll(".pop-active");
+    activePops.length == 0 ? (popWrapper.classList.add("slide-in"), popWrapper.classList.remove("slid")) : (popWrapper.classList.add("slid"), popWrapper.classList.remove("slide-in"));
     for (let h = 0; h < navPop.length; h++) {
       const tab = navPop[h];
-      
       if (tab.classList.contains('pop-active')) {
         tab.classList.remove('pop-active');
+        tab.style.transition = "none";
         
       }
     }
     // document.querySelector('.pop-wrapper').classList.add('pop-w');
     // show it/if statament if it is open, close it, if the closebtn is clicked--remove the class to close it
     // remove transition nav-pop and hide it disp none. none
-
     let targetTab = clk.getAttribute('data-pop');
+    popWrapper.classList.contains("slide-in") ? document.getElementById(targetTab).style.transition = "0.3s ease" : "";
     document.getElementById(targetTab).classList.add('pop-active');
     g.preventDefault();
   });
@@ -29,6 +32,7 @@ dataOpeners.forEach((clk) => {
 dataClose.forEach((c) => {
   c.addEventListener('click', function (y) {
     let closer = c.getAttribute('data-closes');
+    document.getElementById(closer).style.transition = "0.3s ease";
     document.getElementById(closer).classList.remove('pop-active');
     y.preventDefault();
   });
